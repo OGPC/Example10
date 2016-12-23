74ba103eb7e8155449b3029a57f2a1191e231830
Gabriel Kulp <gabrielkulp@gmail.com>
2016-12-23 12:18:43 -0800

    Added Planet Prototype
    The car now has a planet to drive on! A placeholder skybox was also added.

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -7,11 +7,12 @@ public class CamFollow : MonoBehaviour {
 
 	void Start () {
 		transform.position = follow.position;
-		transform.rotation = follow.rotation;
+		transform.rotation = Quaternion.LookRotation(follow.forward, transform.position);
 	}
 	
 	void FixedUpdate () {
 		transform.position = Vector3.Lerp(transform.position, follow.position, strength);
-		transform.rotation = Quaternion.Lerp(transform.rotation, follow.rotation, strength);
+		Quaternion targetRotation = Quaternion.LookRotation(Vector3.Cross(Vector3.Cross(follow.position, follow.forward), follow.position), follow.position);
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, strength);
 	}
 }
